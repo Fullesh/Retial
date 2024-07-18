e7fb138940c3a4e13ff66ea1382cac019aee0360
Danila Belov <danilabelov.adm@yandex.ru>
2024-07-18 06:11:56 +0300

    Добавлена модель завода

--- retail/models.py
+++ retail/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Factory(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    email = models.EmailField(verbose_name='E-mail')
+    country = models.CharField(max_length=30, verbose_name='Страна')
+    city = models.CharField(max_length=30, verbose_name='Город')
+    street = models.CharField(max_length=30, verbose_name='Улица')
+    apartment = models.CharField(max_length=30, verbose_name='Номер дома')
