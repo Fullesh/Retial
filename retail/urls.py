63721cd481313df00ceccdedfb77713756a3751a
Danila Belov <danilabelov.adm@yandex.ru>
2024-07-20 15:41:14 +0300

    Добавлены URL стартовой страницы сети

--- retail/urls.py
+++ retail/urls.py
@@ -1,3 +1,10 @@
+from django.urls import path
+
 from retail.apps import RetailConfig
+from retail.views import NetworkRetrieveAPIView
 
 app_name = RetailConfig.name
+
+urlpatterns = [
+    path('', NetworkRetrieveAPIView.as_view(), name='network_list'),
+]
