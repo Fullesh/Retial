126dac61eacf8dd3cbd450515c9e73b11d11477e
Danila Belov <danilabelov.adm@yandex.ru>
2024-07-20 15:37:17 +0300

    Внесены в админ-панель модели Network и Product

--- retail/admin.py
+++ retail/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from retail.models import Network, Product
+
+
 # Register your models here.
+
+@admin.register(Network)
+class NetworkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'country', 'city', 'supplier', 'debt', 'link_type',)
+    list_filter = ('city', )
+    actions = ['zero_debt']
+
+    def zero_debt(self, request, queryset):
+        queryset.update(debt=0)
+
+    zero_debt.short_description = 'Обнулить задолженность'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'product_description',)
