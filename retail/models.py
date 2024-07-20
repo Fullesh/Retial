83f19660ef92985cec2118cd401967f212b73fe6
Danila Belov <danilabelov.adm@yandex.ru>
2024-07-20 15:38:36 +0300

    Создана модель сети

--- retail/models.py
+++ retail/models.py
@@ -15,3 +15,29 @@ class Product(models.Model):
         verbose_name_plural = 'Продукты'
 
 
+class Network(models.Model):
+    link_types = (
+        (0, 'Завод'),
+        (1, 'Розничная сеть'),
+        (2, 'Индивидуальный предприниматель')
+    )
+    name = models.CharField(max_length=50, verbose_name='Название')
+    email = models.EmailField(verbose_name='E-mail')
+    country = models.CharField(max_length=30, verbose_name='Страна')
+    city = models.CharField(max_length=30, verbose_name='Город')
+    street = models.CharField(max_length=30, verbose_name='Улица')
+    apartment = models.CharField(max_length=30, verbose_name='Номер дома')
+    products = models.ManyToManyField(Product, related_name='networks')
+    supplier = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name='Поставщик', **NULLABLE)
+    debt = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Задолженность перед поставщиком р., кк.',
+                               default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    link_type = models.IntegerField(choices=link_types, verbose_name='Уровень в иерархии')
+
+    def __str__(self):
+        return f'{self.name}: {self.email}, {self.country}, {self.city}, {self.supplier}, {self.debt}, {self.link_type}'
+
+    class Meta:
+        verbose_name = 'Сеть'
+        verbose_name_plural = 'Сети'
+
