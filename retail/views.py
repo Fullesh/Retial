7dbf4a86d201bc29e548e45e1f51953a178ef2e7
Danila Belov <danilabelov.adm@yandex.ru>
2024-07-20 15:42:16 +0300

    Добавлен контроллер детального отображения элемента сети

--- retail/views.py
+++ retail/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.generics import RetrieveAPIView
+
+from retail.models import Network
+from retail.serializers import NetworkSerializer
+
 
 # Create your views here.
+
+class NetworkRetrieveAPIView(RetrieveAPIView):
+    serializer_class = NetworkSerializer
+    queryset = Network.objects.all()
+
