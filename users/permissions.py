1bb682a77508c7c0d4436794646626b923af0105
Fullesh <76729342+Fullesh@users.noreply.github.com>
2024-07-21 18:51:39 +0300

    Delete users/permissions.py

--- users/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsCreator(BasePermission):
-
-    def has_permission(self, request, view):
-        return request.user.email == view.get_object().email
