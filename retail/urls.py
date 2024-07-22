bcb585aa774ba0e61a7bf319eae017eea467cbce
Danila Belov <danilabelov.adm@yandex.ru>
2024-07-22 20:45:13 +0300

    Внесены URL приложения Retail

--- retail/urls.py
+++ retail/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from retail.apps import RetailConfig
-from retail.views import NetworkRetrieveAPIView
+from retail.views import NetworkRetrieveAPIView, NetworkListAPIView, NetworkUpdateAPIView, NetworkDeleteAPIView
 
 app_name = RetailConfig.name
 
 urlpatterns = [
-    path('', NetworkRetrieveAPIView.as_view(), name='network_list'),
+    path('', NetworkListAPIView.as_view(), name='network_list'),
+    path('network/<int:pk>/', NetworkRetrieveAPIView.as_view(), name='network_detail'),
+    path('network/edit/<int:pk>/', NetworkUpdateAPIView.as_view(), name='network_edit'),
+    path('network/update/<int:pk>/', NetworkUpdateAPIView.as_view(), name='network_update'),
+    path('network/delete/<int:pk>/', NetworkDeleteAPIView.as_view(), name='network_delete'),
 ]
