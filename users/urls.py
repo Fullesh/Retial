4818b0c46e95da311d42907eaa1ee47737e6919d
Danila Belov <danilabelov.adm@yandex.ru>
2024-07-22 20:52:16 +0300

    Внесены URL приложения Users

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserRetrieveAPIView
+from users.views import UserRetrieveAPIView, UserCreateAPIView, UserDestroyAPIView, UserUpdateAPIView, UserListAPIView
 
 app_name = UsersConfig.name
 urlpatterns = [
-    path('', UserRetrieveAPIView.as_view(), name='user_detail'),
+    path('', UserCreateAPIView.as_view(), name='user_create'),
+    path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user_delete'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('view/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_view'),
+    path('list/', UserListAPIView.as_view(), name='user_list'),
 ]
