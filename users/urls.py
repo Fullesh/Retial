104109ff1cb5aa6ed6f46ea7647c465234f159ab
Danila Belov <danilabelov.adm@yandex.ru>
2024-07-22 21:16:08 +0300

    Добавлены страницы входа и обновления токена

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.decorators.cache import cache_page
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import UserRetrieveAPIView, UserCreateAPIView, UserDestroyAPIView, UserUpdateAPIView, UserListAPIView
@@ -11,4 +12,6 @@ urlpatterns = [
     path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
     path('view/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_view'),
     path('list/', cache_page(60)(UserListAPIView.as_view()), name='user_list'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
