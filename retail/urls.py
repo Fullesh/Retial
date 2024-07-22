850d58734323eca2ed897f482c7f295fd8b38448
Danila Belov <danilabelov.adm@yandex.ru>
2024-07-22 21:24:58 +0300

    Добавлен URL для создания сети

--- retail/urls.py
+++ retail/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 from retail.apps import RetailConfig
-from retail.views import NetworkRetrieveAPIView, NetworkListAPIView, NetworkUpdateAPIView, NetworkDeleteAPIView
+from retail.views import NetworkRetrieveAPIView, NetworkListAPIView, NetworkUpdateAPIView, NetworkDeleteAPIView, \
+    NetworkCreateAPIView
 
 app_name = RetailConfig.name
 
@@ -10,6 +11,6 @@ urlpatterns = [
     path('', cache_page(60)(NetworkListAPIView.as_view()), name='network_list'),
     path('network/<int:pk>/', NetworkRetrieveAPIView.as_view(), name='network_detail'),
     path('network/edit/<int:pk>/', NetworkUpdateAPIView.as_view(), name='network_edit'),
-    path('network/update/<int:pk>/', NetworkUpdateAPIView.as_view(), name='network_update'),
     path('network/delete/<int:pk>/', NetworkDeleteAPIView.as_view(), name='network_delete'),
+    path('network/create/', NetworkCreateAPIView.as_view(), name='network_create'),
 ]
